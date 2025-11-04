c0f76d2d22123fcc8e0debcc024062c0448923aa
GuruQuality <tsvetkova.e.i@mail.ru>
2025-11-04 15:14:23 +0300

    Added is2Digits

--- src/main/java/com/methods/stepup/App.java
+++ src/main/java/com/methods/stepup/App.java
@@ -7,6 +7,7 @@ public class App {
         //1 Задание - Возрат дробной части
         double result = Methods.fraction(5.3);
         System.out.println("Дробная часть 5.3: " + Math.round(result * 1000.0) / 1000.0);
+        System.out.println();
         //2 Задание - Сумма 2-х последних знаков числа
         int number=18;
         if (number < 10) {
@@ -14,12 +15,22 @@ public class App {
         } else {
             System.out.println("Сумма последних двух чисел: " + Methods.sumLastNum(number));
         }
+        System.out.println();
         //3 задание Преобразовние символ-цифры в соответствующее ей число
         int symbol=Methods.charToNum('3');
         System.out.println("Результат преобразования числа '3': " + symbol);
+        System.out.println();
         //4 задание Проверка позитивности числа
+        System.out.println("Проверка позитивности числа");
         System.out.println("x=0, результат: " + Methods.isPositive(0));
         System.out.println("x=3, результат: " + Methods.isPositive(3));
         System.out.println("x=-5, результат: " + Methods.isPositive(-5));
+        System.out.println();
+        //5 задание Проверка позитивности числа
+        System.out.println("Проверка двухзначности числа");
+        System.out.println("x=-2, результат: " + Methods.is2Digits(-2));
+        System.out.println("x=2, результат: " + Methods.is2Digits(2));
+        System.out.println("x=12, результат: " + Methods.is2Digits(12));
+        System.out.println("x=100, результат: " + Methods.is2Digits(100));
     }
 }
--- src/main/java/com/methods/stepup/Methods.java
+++ src/main/java/com/methods/stepup/Methods.java
@@ -35,18 +35,39 @@ public class Methods {
         int secondDigit = (x / 10) % 10;// (/10)удаляем полседенее число
         return x = lastDigit + secondDigit;
     }
+
     //Преобразовние символ-цифры в соответствующее ей число -Подсказка: код символа ‘0’ — это число 48
-    public static int charToNum(char x){
-        return x-48;
+    public static int charToNum(char x) {
+        return x - 48;
     }
+
     // Проверка позитивности числа
-    public static boolean isPositive(int x){
-        if (x>0) {
+    public static boolean isPositive(int x) {
+        if (x > 0) {
             return true;
-        }else {
+        } else {
             return false;
         }
     }
+
+    //Проверка двухзначности числа
+    public static boolean is2Digits(int x) {
+        if (x <= 0) {
+            System.out.println("Число не положительное");
+            return false;
+        } else {
+            if (x<10){
+                return false;
+            }
+            if (x<100) {
+                return true;
+            } else {
+                return false;
+            }
+        }
+    }
 }
+
+
 
 
