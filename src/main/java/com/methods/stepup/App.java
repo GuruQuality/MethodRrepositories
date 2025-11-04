1f8ad32e0ee13d6d0ac0682f43196c8925f37adf
GuruQuality <tsvetkova.e.i@mail.ru>
2025-11-04 21:17:30 +0300

    Added sum3

--- src/main/java/com/methods/stepup/App.java
+++ src/main/java/com/methods/stepup/App.java
@@ -69,6 +69,6 @@ public class App {
         System.out.println();
         */
 
-        Chapper2Lesson5.run();
+        Chapper2Lesson6.run();
     }
 }
---
+++ src/main/java/com/methods/stepup/Chapper2Lesson6.java
@@ -0,0 +1,31 @@
+package com.methods.stepup;
+
+public class Chapper2Lesson6 {
+    public static void run() {
+        //6 Возврат true, если сумма 2-х равна третьему
+        int x = 8;
+        int y = -1;
+        int z = 4;
+        System.out.println(" x = " + x + " y = " + y + " z = " + z + "\nрезультат: " + Chapper2Lesson6.sum3(x, y, z));
+    }
+
+    //Возврат true, если сумма 2-х равна третьему
+    public static boolean sum3(int x, int y, int z) {
+        if (x + y == z) {
+            return true;
+        }
+        if (y + z == x) {
+            return true;
+        }
+        if (z + x == y) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+}
+
+
+
+
+
