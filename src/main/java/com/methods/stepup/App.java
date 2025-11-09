d837a7a9540bd4e2522eb4163d0f709a9c356ca1
GuruQuality <tsvetkova.e.i@mail.ru>
2025-11-10 00:28:43 +0300

    Added Chapper5_CourseProject

--- src/main/java/com/methods/stepup/App.java
+++ src/main/java/com/methods/stepup/App.java
@@ -1,9 +1,7 @@
 package com.methods.stepup;
 
-import com.methods.stepup.Chapper5_Exercise1;
-
 public class App {
     public static void main(String[] args) {
-        Chapper5_Exercise10.start();
+        Chapper5_CourseProject.start();
     }
 }
---
+++ src/main/java/com/methods/stepup/Chapper5_CourseProject.java
@@ -0,0 +1,25 @@
+package com.methods.stepup;
+
+import java.io.File;
+import java.util.Scanner;
+
+public class Chapper5_CourseProject {
+    public static void start() {
+        int count = 1;
+        while (true) {
+            System.out.println("Введите путь к файлу");
+            String path = new Scanner(System.in).nextLine();
+            File file = new File(path);
+            boolean objectExists = file.exists();//Проверяем сущетсвет ли путь
+            boolean isDirectory = file.isDirectory();//Проверяем что объект является папкой
+            if (objectExists && !isDirectory) {
+                System.out.println("Путь указан верно");
+                System.out.println("Это файл номер " + count);
+            } else {
+                System.out.println("Путь указан не верно");
+                continue;
+            }
+            count += 1;
+        }
+    }
+}
