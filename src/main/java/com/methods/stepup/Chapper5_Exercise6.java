b9c236391fc390abda507e3d84fee7930e15d8ea
GuruQuality <tsvetkova.e.i@mail.ru>
2025-11-09 20:58:57 +0300

    Added equalNum

---
+++ src/main/java/com/methods/stepup/Chapper5_Exercise6.java
@@ -0,0 +1,21 @@
+package com.methods.stepup;
+
+public class Chapper5_Exercise6 {
+    public static void start() {
+        //Циклы #5 Проверка на одинаковость числа
+        int x = 11122;
+        System.out.println("x = " + x + "\nрезультат: " + Chapper5_Exercise6.equalNum(x));
+    }
+
+    //Циклы #5 Проверка на одинаковость числа
+    public static boolean equalNum(int x) {
+        int lastDigit = x % 10;
+        while (x != 0) {
+            if (x % 10 != lastDigit) {
+                return false;
+            }
+            x = x / 10;
+        }
+        return true;
+    }
+}
