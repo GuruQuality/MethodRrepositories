4657895717e4ad1986bb8af3d7687d8d41176f1b
GuruQuality <tsvetkova.e.i@mail.ru>
2025-11-09 17:40:18 +0300

    Added reverseListNums

--- src/main/java/com/methods/stepup/App.java
+++ src/main/java/com/methods/stepup/App.java
@@ -4,6 +4,6 @@ import com.methods.stepup.Chapper5_Exercise1;
 
 public class App {
     public static void main(String[] args) {
-        Chapper5_Exercise1.start();
+        Chapper5_Exercise2.start();
     }
 }
---
+++ src/main/java/com/methods/stepup/Chapper5_Exercise2.java
@@ -0,0 +1,23 @@
+package com.methods.stepup;
+
+public class Chapper5_Exercise2 {
+    public static void start() {
+        //Циклы #2 Запоминание чисел в обратном подрядке
+        int x = 10;
+        System.out.println(" x = " + x + "\nрезультат: " + Chapper5_Exercise2.reverseListNums(x));
+    }
+
+    //Циклы #2 Запоминание чисел в обратном подрядке
+    public static String reverseListNums(int x) {
+        String res = "";
+        if (x < 0) {
+            res = "x может принимать значения от 0 и выше";
+        } else {
+            for (int i = x; i >= 0; i--) {  // ← исправлено на res += x
+                res += i + " ";
+            }
+        }
+        return res;
+    }
+
+}
