5b2944af37693121f3e229b63bada975e9176e0a
GuruQuality <tsvetkova.e.i@mail.ru>
2025-11-09 21:51:49 +0300

    Added square

---
+++ src/main/java/com/methods/stepup/Chapper5_Exercise7.java
@@ -0,0 +1,20 @@
+package com.methods.stepup;
+
+public class Chapper5_Exercise7 {
+    public static void start() {
+        //Циклы #7 Квадрат
+        int x = 4;
+        System.out.println("x = " + x + "\nрезультат: ");
+        Chapper5_Exercise7.square(x);
+    }
+
+    //Циклы #7 Квадрат
+    public static void square(int x) {
+        for (int i = 1; i <= x; i++) {
+            for (int j = 1; j <= x; j++) {
+                System.out.print("*");
+            }
+            System.out.println("");
+        }
+    }
+}
