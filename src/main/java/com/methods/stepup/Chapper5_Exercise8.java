2a3d16a329dd62f88bdc194d16963e2c5ea8a0d3
GuruQuality <tsvetkova.e.i@mail.ru>
2025-11-09 22:11:04 +0300

    Added leftTriangle

---
+++ src/main/java/com/methods/stepup/Chapper5_Exercise8.java
@@ -0,0 +1,22 @@
+package com.methods.stepup;
+
+public class Chapper5_Exercise8 {
+    public static void start() {
+        //Циклы #8 Левый треугольник.
+        int x = 2;
+        System.out.println("x = " + x + "\nрезультат: ");
+        Chapper5_Exercise8.leftTriangle(x);
+    }
+
+    //Циклы #8 Левый треугольник.
+    public static void leftTriangle(int x) {
+        int row = 1;
+        for (int i = 1; i <= x; i++) {//ряд
+            for (int j = 1; j <= row; j++) {//строки
+                System.out.print("*");
+            }
+            System.out.println("");
+            row += 1;
+        }
+    }
+}
