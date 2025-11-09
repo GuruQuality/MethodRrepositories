44582a77fcb36fefa0f56ae1a12232166e2e8524
GuruQuality <tsvetkova.e.i@mail.ru>
2025-11-09 17:50:19 +0300

    Added chet

---
+++ src/main/java/com/methods/stepup/Chapper5_Exercise3.java
@@ -0,0 +1,19 @@
+package com.methods.stepup;
+
+public class Chapper5_Exercise3 {
+    public static void start() {
+        //Циклы #3 Возрат четных чисел числа
+        int x = 9;
+        System.out.println(" x = " + x + "\nрезультат: " + Chapper5_Exercise3.chet(x));
+    }
+
+    //Циклы #3 Возрат четных чисел числа
+    public static String chet(int x) {
+        String res = "";
+        for (int i = 0; i <= x; i+=2) {  // ← исправлено на res += x
+            res += i + " ";
+        }
+        return res;
+    }
+
+}
