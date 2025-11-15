bfc7b9516ff00794785765ea7eff8515bedfffc0
Katerina <tsvetkova.e.i@mail.ru>
2025-11-15 22:11:07 +0300

    Change reverse

--- src/main/java/com/methods/stepup/array/Chapper5_Part2_Array_Exercise6.java
+++ src/main/java/com/methods/stepup/array/Chapper5_Part2_Array_Exercise6.java
@@ -1,19 +1,23 @@
 package com.methods.stepup.array;
 
+import java.util.Arrays;
+
 public class Chapper5_Part2_Array_Exercise6 {
     public static void start() {
         int[] arr = {1, 2, 3, 4, 5};
         //Вызываем метод и получаем результат
         System.out.println("Массив задом-наперед ");
         Chapper5_Part2_Array_Exercise6.reverse(arr);
+        System.out.println(Arrays.toString(arr));
     }
 
     //6 Реверс
     public static void reverse(int arr[]) {
-        int arr2[] = new int [arr.length];
-        for (int i = 0; i < arr.length; i++) {
-            System.out.print(arr2[i]=arr[arr.length-1-i]);
-            System.out.print(" ");
+        for (int i = 0; i < arr.length/2; i++) {
+            // Меняем местами элементы с начала и конца
+            int num=arr[i];
+            arr[i]=arr[arr.length-1 -i];
+            arr[arr.length-1 -i]=num;
         }
     }
 }
